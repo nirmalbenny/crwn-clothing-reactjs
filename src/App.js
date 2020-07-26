174b63ccb921f37e28464ca2d0ce171230c6805d
nirmalbenny <nirmalbennyinbox@gmail.com>
2020-07-26 22:29:14 +0530

    Basic routing is setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from "react";
-import logo from "./logo.svg";
 import HomePage from "./page/homepage/homepage.component";
+import { Route, Switch, Link } from "react-router-dom";
 import "./App.css";
 
+const HatsPage = () => {
+  return (
+    <div>
+      <h1>Hats Page</h1>
+    </div>
+  );
+};
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
+
+      {/* <HomePage /> */}
     </div>
   );
 }
