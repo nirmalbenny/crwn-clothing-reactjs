8e3a6d5697b4a06cfcf5962a949ee1f7d435ca90
nirmalbenny <nirmalbennyinbox@gmail.com>
2020-08-17 23:02:20 +0530

    reselect is used in application, replaced cart selectors with memorized selectors

--- src/component/cart-dropdown/cart-dropdown.component.jsx
+++ src/component/cart-dropdown/cart-dropdown.component.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import CustomButton from "../custom-button/custom-button.component";
 import CartItem from "../cart-item/cart-item.component";
+import { selectCartItems } from "../../redux/cart/cart.selectors";
 import "./cart-dropdown.styles.scss";
 
 const CartDropdown = ({ cartItems }) => {
@@ -17,9 +18,9 @@ const CartDropdown = ({ cartItems }) => {
   );
 };
 
-const mapStateToProps = ({ cart: { cartItems } }) => {
+const mapStateToProps = (state) => {
   return {
-    cartItems,
+    cartItems: selectCartItems(state),
   };
 };
 export default connect(mapStateToProps)(CartDropdown);
